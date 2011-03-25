6f4429b2fe7a70ef64517daf3b66986df3ac43fc
Matt Conway <wr0ngway@yahoo.com>
2011-03-25 19:10:15 -0400

    getting gwo to work with ga (both need to be async js)

--- lib/analytical/modules/google_optimizer.rb
+++ lib/analytical/modules/google_optimizer.rb
@@ -12,28 +12,29 @@ module Analytical
         init_location(location) do
           js = <<-HTML
           <!-- Analytical Init:  Google Website Optimizer Control Script -->
-      		<script>
+          <script>
       		function utmx_section(){}function utmx(){}
       		(function(){var k='#{options[:key]}',d=document,l=d.location,c=d.cookie;function f(n){
-      		if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return c.substring(i+n.
-      		length+1,j<0?c.length:j)}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;
+      		if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return escape(c.substring(i+n.
+      		length+1,j<0?c.length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;
       		d.write('<sc'+'ript src="'+
       		'http'+(l.protocol=='https:'?'s://ssl':'://www')+'.google-analytics.com'
       		+'/siteopt.js?v=1&utmxkey='+k+'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='
       		+new Date().valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
       		'" type="text/javascript" charset="utf-8"></sc'+'ript>')})();
-      		</script>
+      		</script><script>utmx("url",'A/B');</script>
       		<!-- End of Google Website Optimizer Control Script -->
       		<!-- Analytical Init: Google Website Optimizer Tracking Script -->
           <script type="text/javascript">
-          if(typeof(_gat)!='object')document.write('<sc'+'ript src="http'+
-          (document.location.protocol=='https:'?'s://ssl':'://www')+
-          '.google-analytics.com/ga.js"></sc'+'ript>')</script>
-          <script type="text/javascript">
-          try {
-            var gwoTracker=_gat._getTracker("#{options[:account]}");
-            gwoTracker._trackPageview("/#{options[:key]}/test");
-            }catch(err){}</script>
+            var _gaq = _gaq || [];
+            _gaq.push(['gwo._setAccount', '#{options[:account]}']);
+            _gaq.push(['gwo._trackPageview', '/#{options[:key]}/test']);
+            (function() {
+              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+          </script>
           <!-- End of Google Website Optimizer Tracking Script -->
           HTML
           js
@@ -44,14 +45,15 @@ module Analytical
         js = <<-HTML
         <!-- Analytical Init:  Google Website Optimizer Conversion Script -->
         <script type="text/javascript">
-        if(typeof(_gat)!='object')document.write('<sc'+'ript src="http'+
-        (document.location.protocol=='https:'?'s://ssl':'://www')+
-        '.google-analytics.com/ga.js"></sc'+'ript>')</script>
-        <script type="text/javascript">
-        try {
-          var gwoTracker=_gat._getTracker("#{options[:account]}");
-          gwoTracker._trackPageview("/#{options[:key]}/goal");
-          }catch(err){}</script>
+          var _gaq = _gaq || [];
+          _gaq.push(['gwo._setAccount', '#{options[:account]}']);
+          _gaq.push(['gwo._trackPageview', '/#{options[:key]}/goal']);
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        </script>
         <!-- End of Google Website Optimizer Conversion Script -->
         HTML
         js
